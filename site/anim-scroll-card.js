8d4d84cdc1e0f050a7586cda9c51d0e35fcde9b0
FonfonHaha <coureau.paco@gmail.com>
2025-11-01 23:27:33 +0100

    animation on cards

---
+++ site/anim-scroll-card.js
@@ -0,0 +1,11 @@
+const animScrolls = document.querySelectorAll(".anim-scroll-card");
+
+window.addEventListener("scroll", () => {
+    animScrolls.forEach(animScroll => {
+        if (animScroll.getBoundingClientRect().top < window.innerHeight * 0.8) {
+            animScroll.classList.add("visible");
+        } else {
+            animScroll.classList.remove("visible");
+        }
+    });
+});
